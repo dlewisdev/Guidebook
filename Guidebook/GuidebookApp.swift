cfad44fbf9594d3443c1650c19e06b31ddb6248c
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-12 13:11:33 -0500

    Initial Commit

---
+++ Guidebook/GuidebookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuidebookApp.swift
+//  Guidebook
+//
+//  Created by Danielle Lewis on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct GuidebookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
